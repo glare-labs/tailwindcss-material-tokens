0e40da9c06617fd32fe010833b7a2faf06e63e95
bre 97 <by.sgatn3@Gmail.com>
2024-03-30 13:56:45 +0800

    chore: remove cjs supported

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,15 @@
 import typescript from '@rollup/plugin-typescript';
-import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
 
 export default {
   input: './src/index.ts',
   output: [
     {
-      file: './build/index.cjs.js',
-      format: 'cjs',
-    },
-    {
-      file: './build/index.esm.js',
-      format: 'es',
+      file: './build/index.js',
+      format: 'esm',
     },
   ],
   plugins: [
-    commonjs(), 
     typescript({tsconfig: './tsconfig.json'}),
     terser(),
   ],
